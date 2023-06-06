ec8a5b2396f7698bc6a9e2ab851674edd66d6e67
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2023-06-06 16:12:37 -0500

    [feature/Add] pass the project to mix in elixir

---
+++ convertir_monedas/lib/convertir_monedas.ex
@@ -0,0 +1,18 @@
+defmodule ConvertirMonedas do
+  @moduledoc """
+  Documentation for `ConvertirMonedas`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ConvertirMonedas.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ convertir_monedas/test/convertir_monedas_test.exs
@@ -0,0 +1,8 @@
+defmodule ConvertirMonedasTest do
+  use ExUnit.Case
+  doctest ConvertirMonedas
+
+  test "greets the world" do
+    assert ConvertirMonedas.hello() == :world
+  end
+end
